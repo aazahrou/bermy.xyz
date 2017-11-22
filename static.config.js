6b1da8fbe47e43581de0c506638c08616ce40a42
Louis Galipeau <louis@muloka.com>
2017-11-22 12:18:25 -0400

    removed extra title tag

--- static.config.js
+++ static.config.js
@@ -102,7 +102,6 @@ export default {
       return (
         <Html>
           <Head>
-            <title>.</title>
             <meta charSet="UTF-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
